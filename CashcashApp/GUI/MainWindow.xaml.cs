c1dc2b88e79916de20160c182a3916b4c9d5857c
Nicolasvalade <71518430+Nicolasvalade@users.noreply.github.com>
2022-05-18 00:41:38 +0200

    GUI

---
+++ CashcashApp/GUI/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CashcashApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PageAuthentification auth = new(this);
+            frame.Content = auth;
+        }
+
+        private void frame_ContentRendered(object sender, EventArgs e) // Change le titre de la fenêtre au fil de la navigation 
+        {
+            Page? page = frame.Content as Page;
+            if (page != null)
+            {
+                this.Title = page.Title;
+            }
+
+        }
+
+
+    }
+}
